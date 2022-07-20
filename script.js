6ab39e3aa7b0e31ee2df1b727016887a6c647803
lettyfranca <leeffrance@gmail.com>
2022-07-20 19:22:16 -0300

    Dados são jogados ao atualizar a página

---
+++ script.js
@@ -0,0 +1,34 @@
+/**
+ * Jogo do Dado
+ * @author Letícia França
+ */
+
+//Math.floor arredonda o número
+//Math.random sorteia números aleatórios
+//Math.random sempre inicializa em 0, portanto nesse caso somamos 1,
+//para que o random funcione entre 1 - 6.
+var sorteio = Math.floor(Math.random() * 6 + 1);
+
+switch(sorteio){
+	case 1:
+		document.getElementById("face").src = "face1.png";
+		break;
+	case 2:
+		document.getElementById("face").src = "face2.png";
+		break;
+	case 3:
+		document.getElementById("face").src = "face3.png";
+		break;
+	case 4:
+		document.getElementById("face").src = "face4.png";
+		break;
+	case 5:
+		document.getElementById("face").src = "face5.png";
+		break;
+	case 6:
+		document.getElementById("face").src = "face6.png";
+		break;
+	default:
+		//caso contrário
+		break;
+}
